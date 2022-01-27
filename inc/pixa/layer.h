b23d299215c350684854791c35c8cb21ec26cb0f
JojoGuedel <67550456+JojoGuedel@users.noreply.github.com>
2022-01-27 19:49:52 +0100

    added journal back but without images

--- inc/pixa/layer.h
+++ inc/pixa/layer.h
@@ -1,6 +1,7 @@
 #ifndef PIXA_LAYER_H
 #define PIXA_LAYER_H
 
+#include "Pixa/color.h"
 #include "Pixa/texture.h"
 
 #define LAYER_DEFAULT 0
@@ -8,6 +9,8 @@
 typedef struct
 {
     int layer;
+
+    Color *clear_color;
     Texture * texture;
 } Layer;
 
@@ -15,7 +18,5 @@ int create_layer(int layer, Texture *render_target);
 bool destroy_layer(int id);
 
 void set_layer(int id);
-
-
 
 #endif
--- inc/pixa/params.h
+++ inc/pixa/params.h
@@ -4,9 +4,6 @@
 extern int width;
 extern int height;
 
-extern int screen_width;
-extern int screen_height;
-
 extern double framerate;
 extern double ellapsed_time;
 extern double delta_time;
