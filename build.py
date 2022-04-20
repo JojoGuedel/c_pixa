ec2b86e78cb3318551a7e447842b890c3ba6456f
JojoGuedel <joelguedel04@gmail.com>
2022-04-20 20:22:11 +0200

    build: now works on Linux

--- build.py
+++ build.py
@@ -30,7 +30,8 @@ class Path():
         if self.parent != None:
             return self.parent.full_path() + "/" + self.name
         
-        return self.name
+        else: 
+            return "/" + self.name
 
     def exists(self):
         return os.path.exists(self.full_path())
@@ -87,6 +88,7 @@ class Path():
             return None
 
 PIXA_LIB_WIN = "pixa32s.lib"
+PIXA_LIB_LIN = "libpixa.a"
 
 def build_cmd(program: str, src_files: list[str], inc_path: str = None, out_path: str = None, lib_path: str = None, libs: list[str] = None, definitions: list[str] = None, additional_args: list[str] = None):
     cmd = program
@@ -121,7 +123,7 @@ def build_cmd(program: str, src_files: list[str], inc_path: str = None, out_path
 
 class Win:
     @staticmethod
-    def build_example_debug(example: str, debug):
+    def build_example(example: str, debug):
         global workspace_dir, PIXA_LIB_WIN
 
         if not Path.parse(f"{workspace_dir.full_path()}/examples/{example}/").exists():
@@ -182,6 +184,99 @@ class Win:
         print(f"Executing: {cmd}")
         ret = os.system(cmd)
 
+class Lin:
+    @staticmethod
+    def build_example(example: str, debug):
+        global workspace_dir, PIXA_LIB_LIN
+
+        if not Path.parse(f"{workspace_dir.full_path()}/examples/{example}/").exists():
+            print(f"Example '{example}' could not be found")
+            return
+
+        src_files = [f"{workspace_dir.full_path()}/examples/{example}/*.c"]
+        inc_path = f"{workspace_dir.full_path()}/inc"
+
+        lib_path = f"{workspace_dir.full_path()}/lib/linux"
+        libs = ["m", "GL", "pixa", "glfw", "GLEW"]
+        
+        out_path = Path.parse(f"{workspace_dir.full_path()}/out/linux/{workspace_dir.name}")
+        if not out_path.parent.exists():
+            create = []
+            last = out_path.parent
+
+            while not last.exists():
+                create.append(last.full_path())
+                last = last.parent
+            
+            create.reverse()
+
+            for i in create:
+                os.mkdir(i)
+
+        definitions = ["DEBUG"] if debug else None
+        additional_args = ["-g" if debug else None, "-O0" if debug else "-O3", "-Wall", "-Wno-deprecated-declarations"]
+
+        cmd = build_cmd("clang", src_files, inc_path, out_path.full_path(), lib_path, libs, definitions, additional_args)
+
+        print(f"Executing: {cmd}")
+        ret = os.system(cmd)
+
+    @staticmethod
+    def build_pixa_obj():
+        global workspace_dir
+
+        obj_path = Path.parse(f"{workspace_dir.full_path()}/out/linux/obj")
+        if not obj_path.exists():
+            create = []
+            last = obj_path
+
+            while not last.exists():
+                create.append(last.full_path())
+                print(last.full_path())
+                last = last.parent
+            
+            create.reverse()
+
+            for i in create:
+                os.mkdir(i)
+
+        src_files = [f"{workspace_dir.full_path()}/src/*.c"]
+        inc_path = f"{workspace_dir.full_path()}/inc"
+
+        cmd = build_cmd("clang -c", src_files, inc_path)
+
+        os.chdir(obj_path.full_path())
+
+        print(f"Executing: {cmd}")
+        ret = os.system(cmd)
+    
+    @staticmethod
+    def build_pixa_lib():
+        global workspace_dir, PIXA_LIB_LIN
+
+        obj_files = [f"{workspace_dir.full_path()}/out/linux/obj/*.o"]
+        inc_path = f"{workspace_dir.full_path()}/inc"
+
+        out_path = Path.parse(f"{workspace_dir.full_path()}/lib/linux/{PIXA_LIB_LIN}")
+        if not out_path.parent.exists():
+            create = []
+            last = out_path.parent
+
+            while not last.exists():
+                create.append(last.full_path())
+                last = last.parent
+            
+            create.reverse()
+
+            for i in create:
+                os.mkdir(i)
+
+        cmd = build_cmd(f"llvm-ar -rv {out_path.full_path()}", obj_files)
+
+        print(f"Executing: {cmd}")
+        ret = os.system(cmd)
+
+
 def print_help():
     print("USAGE: python [path to build.py] [options]")
     print()
@@ -207,8 +302,9 @@ def main():
 
         if os.name == "nt":
             Win.build_pixa_lib()
-        # elif os.name == "posix":
-        #     pass
+        elif os.name == "posix":
+            Lin.build_pixa_obj()
+            Lin.build_pixa_lib()
         else:
             print(f"Platform '{os.name}' not suportet!")
             return
@@ -229,9 +325,11 @@ def main():
 
         if os.name == "nt":
             Win.build_pixa_lib()
-            Win.build_example_debug(example, debug)
-        # elif os.name == "posix":
-        #     pass
+            Win.build_example(example, debug)
+        elif os.name == "posix":
+            Lin.build_pixa_obj()
+            Lin.build_pixa_lib()
+            Lin.build_example(example, debug)
         else:
             print(f"Platform '{os.name}' not suportet!")
             return
